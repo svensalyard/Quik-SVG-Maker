0ae5561915f27a148ae76b1a1e2d3cc55ac4b81b
svensalyard <svensalyard@gmail.com>
2024-03-10 21:51:18 -0700

    Added README and Notes

---
+++ README.md
@@ -0,0 +1,33 @@
+# Quik SVG Maker
+
+### Description
+
+A quik svg logo generator. Simply answer some questions, pick a shape, colors, and boom, a .svg logo.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Useage](#useage)
+- [License](#license)
+- [Contact](#Contact)
+
+### Installation
+
+Download the zip file or clone the repository.
+
+### Useage
+
+Open the code in an editor of your choice. Run npm i to install the neccessary packages, then run npm start and answer the questions.
+
+### License
+
+Protected under the: MIT ![License](https://img.shields.io/badge/License-MIT-yellow.svg) [MIT](https://opensource.org/licenses/MIT)
+
+### Contact
+
+Author: Sven Salyard
+
+Email: svensalyar@gmail.com
+
+Github: [My Github](https://github.com/svensalyard)
--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const jest = require('jest');
-const shapes = require('./lib/shapes');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const jest = require("jest");
+const shapes = require("./lib/shapes");
 
-const questions = [{
-    type: 'input',
-    message: 'Please enter your initials (max of 3 characters).',
-    name: 'name',
-    validate(answer) {
-        if(answer == 3) {
-            'Please enter only 3 characters or less!'
-        } else {
-            return true
-        }
-        
-    }
-},
-{
-    type: 'input',
-    message: 'Please enter the color for your initials (name or hexidecimal).',
-    name: 'textcolor'
-},
-{
-    type: 'list',
-    message: 'Please choose a shape for the logo.',
-    name: 'shape',
-    choices: ['circle', 'triangle', 'square']
-},
-{
-    type: 'input',
-    message: 'Please enter a color for your shape (name or hexidecimal).',
-    name: 'shapecolor'
-}];
+//questions array
+const questions = [
+	{
+		type: "input",
+		message: "Please enter your initials (max of 3 characters).",
+		name: "name",
+		validate(answer) {
+			if (answer == 3) {
+				("Please enter only 3 characters or less!");
+			} else {
+				return true;
+			}
+		},
+	},
+	{
+		type: "input",
+		message: "Please enter the color for your initials (name or hexidecimal).",
+		name: "textcolor",
+	},
+	{
+		type: "list",
+		message: "Please choose a shape for the logo.",
+		name: "shape",
+		choices: ["circle", "triangle", "square"],
+	},
+	{
+		type: "input",
+		message: "Please enter a color for your shape (name or hexidecimal).",
+		name: "shapecolor",
+	},
+];
 
+// Render the svg logo by writing the answers and shapes to file
 const renderSVG = async (responses) => {
-	fs.writeFile('logo.svg', shapes(responses), (err) => err && console.error(err));
+	fs.writeFile("logo.svg", shapes(responses), (err) => err && console.error(err));
 };
 
+// Initialize the app by calling questions then feeding them to shapes.js
+// then sending the output to renderSVG
 const init = async () => {
 	const responses = await inquirer.prompt(questions);
 	var shape = responses.shape;
 	await renderSVG(responses);
-	console.log('Generated logo.svg');
+	console.log("Generated logo.svg");
 };
 
 init();
