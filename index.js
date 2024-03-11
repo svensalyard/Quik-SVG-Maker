15249df5286b0436ebd59a2d51217eb4b7a05fee
svensalyard <svensalyard@gmail.com>
2024-03-10 21:04:51 -0700

    Added questions

--- index.js
+++ index.js
@@ -3,4 +3,32 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const shapes = require('./lib/shapes');
 
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'Please enter your initials (max of 3 characters).',
+    name: 'name',
+    validate(answer) {
+        if(answer == 3) {
+            'Please enter only 3 characters or less!'
+        } else {
+            return true
+        }
+        
+    }
+},
+{
+    type: 'input',
+    message: 'Please enter the color for your initials (name or hexidecimal).',
+    name: 'textcolor'
+},
+{
+    type: 'list',
+    message: 'Please choose a shape for the logo.',
+    name: 'shape',
+    choices: ['circle', 'triangle', 'square']
+},
+{
+    type: 'input',
+    message: 'Please enter a color for your shape (name or hexidecimal).',
+    name: 'shapecolor'
+}];
